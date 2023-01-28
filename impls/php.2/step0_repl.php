e3af059738c4972b1c8de579c5e2429aef319006
Grzegorz Rozycki <grozycki@op.pl>
2023-01-28 06:01:17 +0100

    Step 0 - with history

--- impls/php.2/step0_repl.php
+++ impls/php.2/step0_repl.php
@@ -1,7 +1,11 @@
 <?php
+
 declare(strict_types=1);
 
 namespace mal;
+
+const PROMPT = 'user> ';
+const HISTORY_FILE = '.mal_history';
 
 function read_line(string $line): string
 {
@@ -18,10 +22,23 @@ function print_line(string $line): string
     return $line;
 }
 
-while (($line = readline('user> ')) !== false) {
-    $line = read_line($line);
-    $line = eval_line($line);
-    $line = print_line($line);
+if (PHP_SAPI === 'cli') {
+    $history_file = getenv('HOME').DIRECTORY_SEPARATOR.HISTORY_FILE;
+    \readline_read_history($history_file);
 
-    echo "$line\n";
+    while (($line = \readline(PROMPT)) !== false) {
+        $line = trim($line);
+
+        if ($line) {
+            \readline_add_history($line);
+        }
+
+        $line = read_line($line);
+        $line = eval_line($line);
+        $line = print_line($line);
+
+        echo "$line\n";
+    }
+
+    \readline_write_history($history_file);
 }
