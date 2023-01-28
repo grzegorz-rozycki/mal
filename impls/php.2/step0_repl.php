e7ebc53476f75e20d851db79a0c3e68d98777d71
Grzegorz Rozycki <grozycki@op.pl>
2023-01-28 05:47:39 +0100

    Step 0

--- impls/php.2/step0_repl.php
+++ impls/php.2/step0_repl.php
@@ -1,1 +1,27 @@
 <?php
+declare(strict_types=1);
+
+namespace mal;
+
+function read_line(string $line): string
+{
+    return $line;
+}
+
+function eval_line(string $line): string
+{
+    return $line;
+}
+
+function print_line(string $line): string
+{
+    return $line;
+}
+
+while (($line = readline('user> ')) !== false) {
+    $line = read_line($line);
+    $line = eval_line($line);
+    $line = print_line($line);
+
+    echo "$line\n";
+}
